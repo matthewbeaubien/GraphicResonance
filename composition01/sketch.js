da95a387ad39aeb172c8307594841db99abaae8e
Matthew Beaubien <matthewabeaubien@gmail.com>
2020-03-08 16:37:46 -0400

    Composition1

--- composition01/sketch.js
+++ composition01/sketch.js
@@ -22,10 +22,11 @@ function preload() {
 
 
 function setup() {
-  canvas = createCanvas(windowWidth/2, windowWidth/1.125);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  canvas.position(x, y);
+
+  var canvas = createCanvas(windowWidth*0.27, windowWidth*0.48);
+ 
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('compositionContainer');
 
   speechRec = new p5.SpeechRec(lang, showResult);
 
@@ -42,7 +43,8 @@ function setup() {
   fft.setInput(mic);                                
   pixelDensity(2);
   frameRate(24);
-  // background('white');
+  background('white');
+
 
 
 }
@@ -55,17 +57,18 @@ function draw() {
     mic.start();
     var nums = [256, 246.5,238,229.5,221,212.5,204,195.5,187,178.5,170,161.5,153,144.5,136,127.5,119,110.5,102,93.5,85,76.5,68,59.5,51,42.5,34,25.5,17,8.5, 0];
                              
+// background('white');
 
 
 
 for (i=0;i<nums.length;i++) {
   if ((spectrum[0]<nums[i]) && (spectrum[0]>nums[i+1])) {
-      image(imgs0[i], 0, 0, windowWidth/2, windowWidth/1.125);
+      image(imgs0[i], 0, 0, windowWidth*.27, windowWidth*.48);
   }
 }
 for (i=0;i<nums.length;i++) {
   if ((spectrum[5]<nums[i]) && (spectrum[5]>nums[i+1])) {
-      image(imgs[i], 0, 0, windowWidth/2, windowWidth/1.125);
+      image(imgs[i], 0, 0, windowWidth*.27, windowWidth*.48);
   }
 }
 
@@ -127,11 +130,12 @@ for (i=0;i<nums.length;i++) {
 
     if(speechRec.resultValue==true) {
 
+
       textSize(spectrum[0]/2);
       fill('white'); 
       // blendMode(DIFFERENCE);
-      textAlign(CENTER, CENTER);
-      text(speechRec.resultString, width/2-(windowWidth/4), height/2-(windowWidth/2.25), windowWidth/2, windowWidth/1.125);
+      textAlign(LEFT, CENTER);
+      text(speechRec.resultString, 0, 0, windowWidth*.27, windowWidth*.27);
       console.log(speechRec.resultConfidence);
 
     }
