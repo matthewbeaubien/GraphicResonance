c6b79bb199c6cf63fe5e8a712af1af21fea75573
Matthew Beaubien <matthewabeaubien@gmail.com>
2020-03-23 18:16:55 -0400

    responsive

--- composition01/sketch.js
+++ composition01/sketch.js
@@ -4,17 +4,24 @@ let mic, fft;
 let lang = navigator.language;
 let speechRec;
 var imgs0 = [];
-var imgs = [];
+var imgs1 = [];
+var imgs2 = [];
+var imgs3 = [];
+
 var canvas;
 
 
 
 function preload() {
-  for (var i=0; i<30; i++) {
-    imgs0[i] = loadImage("assets/Shape01/LowFrequencyBubbles"+i+".png"); 
+
+    for (var i=0; i<30; i++) {
+    imgs1[i] = loadImage("assets/Shape02/LowFrequencyBubbles"+i+".png"); 
   }
   for (var i=0; i<30; i++) {
-    imgs[i] = loadImage("assets/Shape02/HighFrequencyBubbles"+i+".png"); 
+    imgs2[i] = loadImage("assets/Shape03/HighFrequencyBubbles"+i+".png"); 
+  }
+    for (var i=0; i<30; i++) {
+    imgs3[i] = loadImage("assets/Shape04/01_"+i+".png"); 
   }
 }
 
@@ -23,9 +30,8 @@ function preload() {
 
 function setup() {
 
-  var canvas = createCanvas(windowWidth*0.27, windowWidth*0.48);
+  var canvas = createCanvas(windowWidth, windowHeight);
  
-  // Move the canvas so it’s inside our <div id="sketch-holder">.
   canvas.parent('compositionContainer');
 
   speechRec = new p5.SpeechRec(lang, showResult);
@@ -42,8 +48,8 @@ function setup() {
   fft = new p5.FFT(.6, 16);
   fft.setInput(mic);                                
   pixelDensity(2);
-  frameRate(24);
-  background('white');
+  frameRate(30);
+
 
 
 
@@ -58,77 +64,41 @@ function draw() {
     var nums = [256, 246.5,238,229.5,221,212.5,204,195.5,187,178.5,170,161.5,153,144.5,136,127.5,119,110.5,102,93.5,85,76.5,68,59.5,51,42.5,34,25.5,17,8.5, 0];
                              
   // fill(spectrum[0], spectrum[8], spectrum[15]);
+  blendMode(NORMAL);
 
-// blendMode(NORMAL);
+if (windowWidth>windowHeight){
 
-for (i=0;i<nums.length;i++) {
-  if ((spectrum[0]<nums[i]) && (spectrum[0]>nums[i+1])) {
-      image(imgs0[i], 0, 0, windowWidth*.27, windowWidth*.48);
-  }
+      for (i=0;i<nums.length;i++) {
+        if ((spectrum[2]<nums[i]) && (spectrum[2]>nums[i+1])) {
+            image(imgs1[i], 0, 0, windowWidth*.27, windowWidth*.48);
+        }
+      }
+      for (i=0;i<nums.length;i++) {
+        if ((spectrum[4]<nums[i]) && (spectrum[4]>nums[i+1])) {
+            image(imgs2[i], 0, 0, windowWidth*.27, windowWidth*.48);
+        }
+      }
+    }else{
+      for (i=0;i<nums.length;i++) {
+        if ((spectrum[2]<nums[i]) && (spectrum[2]>nums[i+1])) {
+            image(imgs1[i], 0, 0, windowWidth, windowWidth*1.777777777777778);
+        }
+      }
+      for (i=0;i<nums.length;i++) {
+        if ((spectrum[4]<nums[i]) && (spectrum[4]>nums[i+1])) {
+            image(imgs2[i], 0, 0, windowWidth, windowWidth*1.777777777777778);
+        }
+      }
+
+    }
+
 }
-for (i=0;i<nums.length;i++) {
-  if ((spectrum[5]<nums[i]) && (spectrum[5]>nums[i+1])) {
-      image(imgs[i], 0, 0, windowWidth*.27, windowWidth*.48);
-  }
-}
-
-}
-// blendMode(MULTIPLY);
-
-//   rect(0, 0, width, height);
 
 
 
-// 	let spectrum = fft.analyze();
-//   	mic.start();
-// 	var nums = [256, 246.5,238,229.5,221,212.5,204,195.5,187,178.5,170,161.5,153,144.5,136,127.5,119,110.5,102,93.5,85,76.5,68,59.5,51,42.5,34,25.5,17,8.5, 0];
-                             
-
-
-
-//   blendMode(NORMAL);
-// background('WHITE');
-
-// // image(img, 0, 0, 1080, 1920);
-
-//   // blendMode(DIFFERENCE);
-
-
-// // 	for (i=0;i<nums.length;i++) {
-// // 	if ((spectrum[5]<nums[i]) && (spectrum[5]>nums[i+1])) {
-// // 			image(imgs[i], 0, 0, 1080, 1920);
-// // 	}
-// // }
-
-// // // for (i=0;i<nums.length;i++) {
-// // // 	if ((spectrum[15]<nums[i]) && (spectrum[15]>nums[i+1])) {
-// // // 			image(imgs2[i], 0, 0, 1080, 1920);
-// // // 	}
-// // // }
-// // // for (i=0;i<nums.length;i++) {
-// // // 	if ((spectrum[15]<nums[i]) && (spectrum[15]>nums[i+1])) {
-// // // 			image(imgs3[i], 0, 0, 1080, 1920);
-// // // 	}
-// // // }
-
-
-// }
-
-
-// function print() {
-//   window.print();
-// }
-
-// function gotSpeech(){
-//   if (speechRec.resultValue){
-//     (speechRec.resultString);
-//   }
-// }
-
-
-
-  function showResult(){
-        let spectrum = fft.analyze();
+function showResult(){
+  
+  let spectrum = fft.analyze();
     mic.start();
 
 
@@ -136,12 +106,15 @@ for (i=0;i<nums.length;i++) {
 
 
       textSize(spectrum[0]/2);
-      // fill(spectrum[15], spectrum[8], spectrum[0]); 
-      // blendMode(ADD);
+      fill('white');
       textAlign(LEFT, CENTER);
-      text(speechRec.resultString, 0, 0, windowWidth*.27, windowWidth*.27);
+      text(speechRec.resultString, 0, 0, windowWidth, windowHeight);
       console.log(speechRec.resultConfidence);
 
     }
+}
 
-  }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+
+}
